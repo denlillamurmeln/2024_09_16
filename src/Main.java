1990c1d666faaad0770540d4e59afa98dbc6f028
denlillamurmeln <rooba87@gmail.com>
2024-09-16 09:46:25 +0200

    started method overloading example

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int score = calculateScore("Robert", 200);
+        System.out.println(score);
+
+        int newScore = calculateScore(300);
+        System.out.println(newScore);
+    }
+
+    public static int calculateScore(String playername, int score){
+        System.out.println("Player " + playername + " score " + score);
+        return score * 1000;
+    }
+
+    public static int calculateScore(int score){
+        System.out.println("Unknown player score " + score);
+        return score * 1000;
     }
 }
